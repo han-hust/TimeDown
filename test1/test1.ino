bf610b5f725bcfebd16df63645ad772977b5b587
han-hust <3268835411@qq.com>
2021-02-28 22:37:12 +0800

    完成M1 button2cmd()

--- test1/test1.ino
+++ test1/test1.ino
@@ -1,19 +1,16 @@
 #define DEFAULE_TIME1 4*60*60
 #define DEFAULE_TIME2 1*60*60
-
+#define HANTEST 1
 
 typedef unsigned long time_t;
 
-void setup() {
-	pinMode(13, OUTPUT);
-}
+enum Cmd {
+	CMD_VOID = 0, CMD_STOP1, CMD_STOP2, CMD_STOPBELL, CMD_RESET, CMD_ESC, CMD_READY, CMD_YES, CMD_BEGIN, CMD_SLEEP, CMD_GOTOZERO
+};
 
-void loop() {
-	digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
-	delay(1000);              // wait for a second
-	digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
-	delay(100);              // wait for a second
-}
+enum Button {
+	BUTTON_VOID = 0, BUTTON_STOP2, BUTTON_STOP1, BUTTON_RESET, BUTTON_INTERFACE, BUTTON_TIME, BUTTON_ESC, BUTTON_YES, BUTTON_BEGIN
+};
 
 class Timer {
 private:
@@ -34,7 +31,7 @@ protected:
 		case  0:
 			return 0;
 		case  1:
-			return timeB + micros() - timeO;
+			return timeB + millis() - timeO;
 		case  2:
 			return timeB;
 		}
@@ -42,7 +39,7 @@ protected:
 
 public:
 	void start() {
-		timeO = micros();
+		timeO = millis();
 		timeB = 0;
 		state = 1;
 	}
@@ -54,7 +51,7 @@ public:
 		case  1:
 			break;
 		case  2:
-			timeO = micros();
+			timeO = millis();
 			state = 1;
 			break;
 		}
@@ -65,7 +62,7 @@ public:
 		case  0:
 			break;
 		case  1:
-			timeB += micros() - timeO;
+			timeB += millis() - timeO;
 			state = 2;
 			break;
 		case  2:
@@ -106,6 +103,13 @@ public:
 		return timeAll - Timer::getTime() / 1000 + timeAdd;
 	}
 
+	time_t getTimeMicro() {
+		if (HANTEST) {
+			return Timer::getTime();
+		}
+		return 0;
+	}
+
 };
 
 class Buttoner {
@@ -135,7 +139,7 @@ public:
 		timer = new TimerBack();
 	}
 
-	virtual void button2cmd(int button, int inter) = 0;
+	virtual Cmd button2cmd(int button, int inter) = 0;
 
 	virtual void do_cmd(int cmd) = 0;
 
@@ -151,7 +155,72 @@ public:
 		defaultTime = DEFAULE_TIME1;
 	}
 
-	void button2cmd(int button, int inter) {} //todo
+	Cmd button2cmd(int button, int inter) {
+
+		switch (button)
+		{
+		case BUTTON_VOID:
+			break;
+		case BUTTON_STOP2:
+			switch (state)
+			{
+			case 3:
+				return CMD_STOP2;
+				break;
+			case 6:
+				return CMD_STOPBELL;
+				break;
+			}
+			break;
+		case BUTTON_STOP1:
+			return CMD_STOP1;
+			break;
+		case BUTTON_RESET:
+			switch (state)
+			{
+			case 4:
+				return CMD_RESET;
+				break;
+			case 5:
+				return CMD_ESC;
+				break;
+			}
+			break;
+		case BUTTON_INTERFACE:
+			break;
+		case BUTTON_TIME:
+			break;
+		case BUTTON_ESC:
+			return CMD_ESC;
+			break;
+		case BUTTON_YES:
+			switch (state)
+			{
+			case 1:
+				return CMD_READY;
+				break;
+			case 5:
+				return CMD_YES;
+				break;
+			}
+			break;
+		case BUTTON_BEGIN:
+			switch (state)
+			{
+			case 2:
+				return CMD_BEGIN;
+				break;
+			case 4:
+				return CMD_BEGIN;
+				break;
+			case 6:
+				return CMD_SLEEP;
+				break;
+			}
+			break;
+		}
+		return CMD_VOID;
+	} //todo
 	void do_cmd(int cmd) {} //todo
 };
 
@@ -162,13 +231,36 @@ public:
 		defaultTime = DEFAULE_TIME2;
 	}
 
-	void button2cmd(int button, int inter) {} //todo
+	Cmd button2cmd(Button button, int inter) {
+	} //todo
 	void do_cmd(int cmd) {} //todo
 };
 
 class Displayer {
 public:
 	void display() { //todo about dev
-		
+
 	}
 };
+
+
+void setup() {
+	pinMode(13, OUTPUT);
+	Serial.begin(9600);
+}
+
+void loop() {
+	digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
+	delay(100);              // wait for a second
+	digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
+	delay(100);              // wait for a second
+	TimerBack *b = new TimerBack();
+	b->reset(100);
+	b->start();
+	delay(2000);
+	Serial.print("hello : ");
+	Serial.print(b->getTime());
+	Serial.print(" * ");
+	Serial.print(b->getTimeMicro());
+	Serial.print("\n");
+}
